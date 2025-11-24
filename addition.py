37b9d349b1b12bacde1436b0c22c7a8e2867b276
shreyamchakrasali-hash <shreyamchakrasali@gmail.com>
2025-11-24 16:46:06 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -4,11 +4,14 @@ def add(a, b):
     return a + b
 
 if name == "main":
-    if len(sys.argv) == 3:  
-        x = int(sys.argv[1])
-        y = int(sys.argv[2])
-    else:  
-        x = 10
-        y = 20
+    # Ensure correct number of arguments
+    if len(sys.argv) != 3:
+        print("Usage: python add.py <num1> <num2>")
+        sys.exit(1)
 
-    print("sum", add(x, y))
+    # Convert arguments to integers
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+
+    # Print the sum
+    print("Sum:", add(x, y))
